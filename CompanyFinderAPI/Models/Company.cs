b55d201f1a5b2e7768c4cb2dd5b907e131da3b06
Wes Matlock <thomasmatlockbba@gmail.com>
2023-10-11 15:51:59 -0500

    Created Fundamentals pages

--- CompanyFinderAPI/Models/Company.cs
+++ CompanyFinderAPI/Models/Company.cs
@@ -3,13 +3,13 @@
     public class Company
     {
         public int Id { get; set; }
-        public string Symbol { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Sector { get; set; }
-        public string Industry { get; set; }
+        public string? Symbol { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Sector { get; set; }
+        public string? Industry { get; set; }
         // Foreign Key for WatchList
         public int? WatchListId { get; set; }
-        public WatchLists WatchList { get; set; }
+        public WatchLists? WatchList { get; set; }
     }
 }
