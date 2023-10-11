c962cd34db309342dbcdfe696a0f5c4c17cb6a03
Wes Matlock <thomasmatlockbba@gmail.com>
2023-10-10 20:27:25 -0500

    Project Setup

---
+++ CompanyFinderAPI/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyFinderAPI.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Sector { get; set; }
+        public string Industry { get; set; }
+    }
+}
--- CompanyFinderAPI/Program.cs
+++ CompanyFinderAPI/Program.cs
@@ -1,16 +1,30 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
 
 var app = builder.Build();
 
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{action=Index}/{id?}",
+        defaults: new { controller = "Home", action = "Index" });
+});
+
 // Configure the HTTP request pipeline.
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
